fa583e7396edd0137c8a3dc0567061c2a1538d9f
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2020-02-14 16:23:23 +0530

    committing resizable array and sln file

--- DSA/Arrays/Program.cs
+++ DSA/Arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Arrays
 {
@@ -6,7 +7,30 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from Arrays");
+            ResizableArrays<int> arr = new ResizableArrays<int>();
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(5);
+
+            // Using List<t>
+            List<string> dynamicArray = new List<string>();
+            dynamicArray.Add("akash");
+            dynamicArray.Add("deepak");
+            dynamicArray.Add("aakash");
+            dynamicArray.Add("zoho");
+
+            dynamicArray.Sort();
+            foreach (string item in dynamicArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Size of resizable array {arr.Count}");
+
+            Console.WriteLine(dynamicArray[2]);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ DSA/Arrays/ResizableArrays.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace Arrays
+{
+    public class ResizableArrays<T>
+    {
+        private T[] _items;
+
+        public ResizableArrays()
+        {
+            
+        }
+
+        public int Count
+        {
+            get
+            {
+                if(this._items == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return this._items.Length;
+                }
+            }
+        }
+
+        public void Add(T item)
+        {
+            if(this._items == null)
+            {
+                this._items = new T[] { item };
+            }
+            else
+            {
+                T[] newArray = new T[this._items.Length + 1]; // initializing a new array with increased size
+                
+                for(int i = 0; i < this._items.Length; i++)
+                {
+                    newArray[i] = this._items[i]; // copying the old array to new array
+                }
+
+                newArray[this._items.Length] = item; // adding the new value at the end of the new array
+
+                this._items = newArray; // assigning the new array to items array
+            }
+        }
+    }
+}
