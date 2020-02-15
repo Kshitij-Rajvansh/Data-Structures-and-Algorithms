7969c5f2a59c577ec89559be52d423d88f503c24
newfangledCoder <kshitij.raj@hotmail.com>
2020-02-15 17:50:10 +0530

    commiting arrays first two question

--- DSA/Arrays/Program.cs
+++ DSA/Arrays/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace Arrays
 {
@@ -7,30 +8,89 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            ResizableArrays<int> arr = new ResizableArrays<int>();
-            arr.Add(1);
-            arr.Add(2);
-            arr.Add(3);
-            arr.Add(5);
+            int[] arr1 = new int[]{1,0,0,2,1,1,0,0,2,1,0,1,0};
 
-            // Using List<t>
-            List<string> dynamicArray = new List<string>();
-            dynamicArray.Add("akash");
-            dynamicArray.Add("deepak");
-            dynamicArray.Add("aakash");
-            dynamicArray.Add("zoho");
+            int[] sortedArr = SortOneAndtwo(arr1);
 
-            dynamicArray.Sort();
-            foreach (string item in dynamicArray)
+            foreach(int i in sortedArr)
             {
-                Console.WriteLine(item);
+                Console.Write(i + "  ");
             }
 
-            Console.WriteLine($"Size of resizable array {arr.Count}");
+            int[] arr2 = new int[]{2,4,7,10,15,25};
 
-            Console.WriteLine(dynamicArray[2]);
+            PrintSumPair(arr2, 17);
 
             Console.ReadLine();
         }
+
+        public static int[] SortOneAndtwo(int[] arr)
+        {
+            int j, countOne = 0, countTwo = 0, countZero = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == 0)
+                {
+                    countZero++;
+                }
+                else if(arr[i] == 1)
+                {
+                    countOne++;
+                }
+                else if(arr[i] == 2)
+                {
+                    countTwo++;
+                }
+            }
+
+            j = 0;
+
+            while(countZero > 0)
+            {
+                arr[j] = 0;
+                countZero--;
+                j++;
+            }
+            while(countOne > 0)
+            {
+                arr[j] = 1;
+                countOne--;
+                j++;
+            }
+            while(countTwo > 0)
+            {
+                arr[j] = 2;
+                countTwo--;
+                j++;
+            }
+
+            return arr;
+        }
+
+        public static void PrintSumPair(int[] arr, int sum)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[start] + arr[end] == sum)
+                {
+                    Console.WriteLine(arr[start] + " , " + arr[end]);
+                    end--;
+                }
+                else if(arr[start] + arr[end] > sum)
+                {
+                    end--;
+                }
+                else if(arr[start] + arr[end] < sum)
+                {
+                    start++;
+                }
+            }
+        }
+
+        
     }
 }
