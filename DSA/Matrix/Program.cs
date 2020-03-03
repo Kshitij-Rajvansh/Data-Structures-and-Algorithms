72ac6224865a403f666e648513898899736adc9d
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2020-03-03 18:24:21 +0530

    committing matrix code

---
+++ DSA/Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[,]{ {1,2,3}, {4,5,6}, {7,8,9} };
+
+            int sum = MaxSumPath(arr);
+        }
+
+        #region Function to return the max sum path ina 2d array
+            public static int MaxSumPath(int[,] arr)
+            {
+                int i = 0, j = 0;
+                int sum = arr[i ,j];
+
+                while( i < arr.GetLength(0) && j < arr.GetLength(1))
+                {
+                    if(arr[i, j+1] > arr[i+1, j])
+                    {
+                        sum += arr[i, j+1];
+                        j = j+1;
+                    }
+                    else
+                    {
+                        sum += arr[i+1, j];
+                        i = i+1;
+                    }
+                }
+
+                return sum;               
+            }
+        #endregion
+    }
+}
---
+++ DSA/SearchAlgorithms/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SearchAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12 };
+
+            int index = BinarySearch(arr, 11);
+
+            Console.WriteLine(arr[index]);
+
+            Console.ReadLine();
+        }
+
+
+        #region Binary Search
+        /// <summary>
+        /// This method search for the given value in the given integer array. The array must be sorted.
+        /// </summary>
+        /// <param name="arr">An integer array to search in</param>
+        /// <param name="searchValue">The value to search for</param>
+        /// <returns></returns>
+        public static int BinarySearch(int[] arr, int searchValue)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while(high >= low)
+            {
+                int mid = low + (high - low) / 2;
+
+                if(arr[mid] == searchValue)
+                {
+                    return mid;
+                }
+                else if(arr[mid] < searchValue)
+                {
+                    low = mid + 1;
+                }
+                else if(arr[mid] > searchValue)
+                {
+                    high = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+        #endregion
+    }
+}
