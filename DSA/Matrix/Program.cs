430bb9964d3099da0f2b43dbfdadbc3221f88595
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2020-03-04 16:38:22 +0530

    added DP logic to max sum path

--- DSA/Matrix/Program.cs
+++ DSA/Matrix/Program.cs
@@ -6,54 +6,43 @@ namespace matrix
     {
         static void Main(string[] args)
         {
-            int[,] arr = new int[,]{ {1,2,4}, {3,4,5}, {8,6,9} };
+            int[,] arr = new int[,]{ {1,1000,2}, {500,100,60}, {70000,800,90} };
 
             int sum = MaxSumPath(arr);
+
+            Console.Write(sum);
         }
 
         #region Function to return the max sum path ina 2d array
             public static int MaxSumPath(int[,] arr)
             {
-                int i = 0, j = 0;
-                int sum = arr[i ,j];
+                int numRows = arr.GetLength(0);
+                int numCols = arr.GetLength(1);
+                int[,] arr2 = new int[numRows, numCols];
 
-                while( i < arr.GetLength(0) && j < arr.GetLength(1))
+                for(int i = 0; i < numRows; i++)
                 {
-                    if( i+1 < arr.GetLength(0) && j+1 < arr.GetLength(1))
+                    for(int j = 0; j < numCols; j++)
                     {
-                        if(arr[i, j+1] > arr[i+1, j])
+                        int leftIndex = j - 1 >= 0 ? j-1 : 0;
+                        int topIndex = i - 1 >= 0 ? i -1: 0;
+
+                        int leftVal = j == 0 ? 0 : arr2[i,leftIndex];
+                        int topVal = i == 0 ? 0 : arr2[topIndex,j];
+
+                        if(j == 0 && i == 0)
                         {
-                            sum += arr[i, j+1];
-                            j = j+1;
+                            arr2[i, j] = arr[i, j];
                         }
                         else
                         {
-                            sum += arr[i+1, j];
-                            i = i+1;
-                        }
-                    }
-                    else
-                    {
-                        if(i+1 >= arr.GetLength(0) )
-                        {
-                            j = j+1;
-                            if(j < arr.GetLength(1))
-                            {
-                                sum += arr[i,j];
-                            }
-                        }
-                        else
-                        {
-                            i = i+1;
-                            if(i < arr.GetLength(0))
-                            {
-                                sum += arr[i,j];
-                            }
+                            int maxVal = arr[i, j] + leftVal > arr[i,j] + topVal ? arr[i, j] + leftVal : arr[i,j] + topVal;
+                            arr2[i,j] = maxVal;                            
                         }
                     }
                 }
 
-                return sum;               
+                return arr2[numRows-1, numCols-1];             
             }
         #endregion
     }
