499bcc9c8e1b8b63ca8ed00d744d39e0826dc3f1
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2020-04-14 16:21:59 +0530

    modification to obj dir

---
+++ DSA/Sorting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 2, 4, 9, 10, 7 };
+
+            Console.WriteLine("Before : [ 3, 5, 2, 4, 9, 10, 7 ]");
+
+            SortingAlgorithms.InsertionSort(arr);
+
+            foreach (int num in arr)
+            {
+                Console.Write(num + " , ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DSA/Sorting/SortingAlgorithms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    public static class SortingAlgorithms
+    {
+        #region Bubble Sort
+        public static void BubbleSort(int[] arr)
+        {
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+
+                        swapped = true;
+                    }
+                }
+            } while (swapped == true);
+        }
+        #endregion
+
+        #region Insertion Sort
+        public static void InsertionSort(int[] arr)
+        {
+            int newValue;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                newValue = arr[i];
+
+                int j = i;
+
+                while (j > 0 && arr[j-1] > newValue)
+                {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[j] = newValue;
+            }
+        }
+        #endregion
+    }
+}
